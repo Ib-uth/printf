0890971d8a2a22165f45ef5d2d4e7b2b66854d7b
Ibraheem Uthman <iamlonelypoet@gmail.com>
2023-04-25 23:34:26 +0100

    update

--- main.h
+++ main.h
@@ -8,11 +8,11 @@
 #define BUFFER_SIZE 1024
 
 /* FLAGS */
-#define FLAG_MINUS 1
-#define FLAG_PLUS 2
-#define FLAGZERO 4
-#define FLAG_HASH 8
-#define FLAG_SPACE 16
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
 
 /* SIZES */
 #define S_LONG 2
